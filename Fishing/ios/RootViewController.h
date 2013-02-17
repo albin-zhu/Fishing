682af6484cd266e847f63a7d4067ad0f7b0ec6fa
albin <albinyewen@gmail.com>
2013-02-17 16:09:35 +0800

    Initial Commit

---
+++ Fishing/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FishingAppController.h
+//  Fishing
+//
+//  Created by 朱亚斌 on 13-2-17.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Fishing/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fishing
+//
+//  Created by 朱亚斌 on 13-2-17.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
